e076b2c824b73821b62c403e625c18e5437775c5
Sina Sadeghi <sadeq.s502@gmail.com>
2026-05-14 17:15:24 +0200

    skeleton gin router and server setup and database connection

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // Postgres driver
+)
+
+type Book struct {
+	ID     int    `json:"id" db:"id"`
+	Title  string `json:"title" db:"title"`
+	Author string `json:"author" db:"author"`
+}
+
+func main() {
+	// Connect to Docker Database
+	dsn := "user=user password=password dbname=library_db sslmode=disable"
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Setup Gin Router
+	r := gin.Default()
+
+	// Define a Route
+	r.GET("/books", func(c *gin.Context) {
+		var books []Book
+		err := db.Select(&books, "SELECT id, title, author FROM books")
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(200, books)
+	})
+
+	// Start Server
+	r.Run(":8080")
+}
